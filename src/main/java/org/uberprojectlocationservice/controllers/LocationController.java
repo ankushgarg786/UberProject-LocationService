3d965cce2a18734be7d6b3b32eba0f9725824141
Ankush Garg <ankushgarg7123@gmail.com>
2025-07-06 23:25:39 +0530

    Just formatted the code

--- src/main/java/org/uberprojectlocationservice/controllers/LocationController.java
+++ src/main/java/org/uberprojectlocationservice/controllers/LocationController.java
@@ -14,9 +14,7 @@ import java.util.List;
 @RequestMapping("/api/v1/location")
 public class LocationController {
 
-
     private final LocationService locationService;
-
 
     public LocationController(LocationService locationService) {
         this.locationService = locationService;
@@ -25,11 +23,11 @@ public class LocationController {
     @PostMapping("/drivers")
     public ResponseEntity<Boolean> saveDriverLocation(@RequestBody SaveDriverLocationRequestDTO saveDriverLocationRequestDTO) {
 
-        try{
-            Boolean response=locationService.saveDriverLocation(saveDriverLocationRequestDTO.getDriverId(),saveDriverLocationRequestDTO.getLatitude(),saveDriverLocationRequestDTO.getLongitude());
+        try {
+            Boolean response = locationService.saveDriverLocation(saveDriverLocationRequestDTO.getDriverId(), saveDriverLocationRequestDTO.getLatitude(), saveDriverLocationRequestDTO.getLongitude());
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
 
-        }catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(false);
         }
 
@@ -38,7 +36,7 @@ public class LocationController {
     @GetMapping("/nearby/drivers")
     public ResponseEntity<List<DriverLocationDTO>> getNearbyDrivers(@RequestBody NearByDriversRequestDTO nearByDriversRequestDTO) {
         try {
-            List<DriverLocationDTO> drivers=locationService.getNearbyDrivers(nearByDriversRequestDTO.getLatitude(),nearByDriversRequestDTO.getLongitude());
+            List<DriverLocationDTO> drivers = locationService.getNearbyDrivers(nearByDriversRequestDTO.getLatitude(), nearByDriversRequestDTO.getLongitude());
             return ResponseEntity.status(HttpStatus.OK).body(drivers);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
